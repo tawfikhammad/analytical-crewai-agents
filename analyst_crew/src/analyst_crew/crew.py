49b5f13d7ea1b03fdfee54d907412b5b989f662e
tawfikhammad <tawfikhammad@icloud.com>
2025-08-30 22:56:55 +0300

    add validation functions for research and summary results

--- analyst_crew/src/analyst_crew/crew.py
+++ analyst_crew/src/analyst_crew/crew.py
@@ -1,10 +1,47 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task, TaskOutput
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
-from typing import List
+from typing import List, Tuple, Any
 from crewai_tools import SerperDevTool
+import json
 
-search_tool = SerperDevTool(n_results=5)
+search_tool = SerperDevTool(n_results=3)
+
+def validate_research_result(result: TaskOutput) -> Tuple[bool, Any]:
+    try:
+        result = result.raw
+        #check word count
+        word_count = len(result.split())
+        if not result or word_count < 400:
+            return (False, "Summary output is short (less than 400 words)")
+        
+        return (True, result.strip())
+
+    except Exception as e:
+        return (False, "Unexpected error during validation")
+
+def validate_summary_result(result: TaskOutput) -> Tuple[bool, Any]:
+    try:
+
+        result = result.raw
+        #Check word count
+        word_count = len(result.split())
+        if not result or word_count < 150:
+            return (False, "Summary output is short (less than 150 words)")
+
+        if word_count > 400:
+            return (False, "Summary output is too long (more than 400 words)")
+        # Check sentence count
+        sentences = [s.strip().lower() for s in result.split('.') if len(s.strip()) > 10]
+        if len(sentences) > 7:
+            return (False, "Summary output should contain at most 7 meaningful sentences.")
+
+        if not sentences or len(sentences) < 1:
+            return (False, "Summary output should contain at least 1 meaningful sentences.")
+
+        return (True, result.strip())
+    except Exception as e:
+        return (False, "Unexpected error during validation")
 
 @CrewBase
 class AnalystCrew():
@@ -40,12 +77,14 @@ class AnalystCrew():
         return Task(
             config=self.tasks_config['research_task'],
             tools=[search_tool],
+            guardrail=validate_research_result
         )
 
     @task
     def summarize_task(self) -> Task:
         return Task(
             config=self.tasks_config['summarize_task'],
+            guardrail=validate_summary_result
         )
 
     @task
