4099fcc48437d296f430fcc2e7104ae7d32c8036
tawfikhammad <tawfikhammad@icloud.com>
2025-09-03 03:53:13 +0300

    update input topic

--- code_analyst/src/code_analyst/main.py
+++ code_analyst/src/code_analyst/main.py
@@ -1,25 +1,13 @@
-#!/usr/bin/env python
 import sys
 import warnings
-
 from datetime import datetime
-
 from code_analyst.crew import CodeAnalyst
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
 def run():
-    """
-    Run the crew.
-    """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'topic': 'What is the churn rate in France country?',
     }
     
     try:
